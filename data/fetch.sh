a7afe0a31620cee4d1d9c1b4117bd60f5d2e45bc
Roderic Page <rdmpage@gmail.com>
2021-04-08 13:00:07 +0100

    Tweaks to fetching

--- data/fetch.sh
+++ data/fetch.sh
@@ -7,17 +7,24 @@
 #rm iBOL_phase_0.50_COI.tsv.zip
 
 # iBOL_phase_0.50_COI.tsv is not UTF-8 encoded
-#iconv -f iso-8859-1 -t utf-8 iBOL_phase_0.50_COI.tsv > iBOL_phase_0.50_COI.tsv.new
-#rm iBOL_phase_0.50_COI.tsv
-#mv iBOL_phase_0.50_COI.tsv.new iBOL_phase_0.50_COI.tsv
+curl http://bins.boldsystems.org/data/datarelease/NewPackages/iBOL_phase_0.50_COI.tsv.zip > iBOL_phase_0.50_COI.tsv.zip
+unzip iBOL_phase_0.50_COI.tsv.zip
+rm iBOL_phase_0.50_COI.tsv.zip
+iconv -f iso-8859-1 -t utf-8 iBOL_phase_0.50_COI.tsv > iBOL_phase_0.50_COI.tsv.new
+rm iBOL_phase_0.50_COI.tsv
+mv iBOL_phase_0.50_COI.tsv.new iBOL_phase_0.50_COI.tsv
 
 curl http://bins.boldsystems.org/data/datarelease/NewPackages/iBOL_phase_0.75_COI.tsv.zip > iBOL_phase_0.75_COI.tsv.zip
 unzip iBOL_phase_0.75_COI.tsv.zip
 rm iBOL_phase_0.75_COI.tsv.zip
 
+# iBOL_phase_1.00_COI.tsv has UTF-8 errors
 curl http://bins.boldsystems.org/data/datarelease/NewPackages/iBOL_phase_1.00_COI.tsv.zip > iBOL_phase_1.00_COI.tsv.zip
 unzip iBOL_phase_1.00_COI.tsv.zip
 rm iBOL_phase_1.00_COI.tsv.zip
+iconv -f iso-8859-1 -t utf-8 iBOL_phase_1.00_COI.tsv > iBOL_phase_1.00_COI.tsv.new
+rm iBOL_phase_1.00_COI.tsv
+mv iBOL_phase_1.00_COI.tsv.new iBOL_phase_1.00_COI.tsv
 
 curl http://bins.boldsystems.org/data/datarelease/NewPackages/iBOL_phase_1.25_COI.tsv.zip > iBOL_phase_1.25_COI.tsv.zip
 unzip iBOL_phase_1.25_COI.tsv.zip
